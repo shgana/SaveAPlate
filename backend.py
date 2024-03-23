2af33072f1b4642d4da0228a7c1debe075ebb94f
Shyam <39486717+shgana@users.noreply.github.com>
2024-03-23 19:53:31 +0000

    food items now only contains the day1 items

--- backend.py
+++ backend.py
@@ -31,24 +31,19 @@ with open('day2.txt', 'w') as file:
     for item in food_items:
         food_name = item.get_text(strip=True)
         file.write(food_name + '\n')
+
+
 with open('day1.txt', 'r') as file:
-    day1_items = set(file.read().strip().split('\n'))
+    day1_items = set(file.read().splitlines())
 
-# Open and read day2.txt into a set
+# Read the contents of day2.txt
 with open('day2.txt', 'r') as file:
-    day2_items = set(file.read().strip().split('\n'))
+    day2_items = set(file.read().splitlines())
 
-# Find items that are unique to day1 (not in day2)
-unique_to_day1 = day1_items - day2_items
+# Find items that are in day 1 but not in day 2
+unique_items = day1_items - day2_items
 
-# Find items that are unique to day2 (not in day1)
-unique_to_day2 = day2_items - day1_items
-
-# Combine the unique items from both days
-differences = unique_to_day1.union(unique_to_day2)
-
-# Optionally, write the differences to a new file
+# Write the unique items to food_items.txt
 with open('food_items.txt', 'w') as file:
-    for item in differences:
+    for item in unique_items:
         file.write(item + '\n')
-
